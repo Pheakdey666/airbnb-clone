1440346a8764aed7871b17fecdbd9f2ceee30152
Man Pheakdey <manpheakdey69@gmail.com>
2023-12-06 20:47:27 -0800

    adding navbar, logo, and set up components folder

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,9 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+
 import "./globals.css";
 import { Nunito } from "next/font/google";
+import Navbar from "./components/navbar/Navbar";
 
-const inter = Inter({ subsets: ["latin"] });
 const font = Nunito({
   subsets: ["latin"],
 });
@@ -19,7 +19,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={font.className}>{children}</body>
+      <body className={font.className}>
+        <Navbar />
+        {children}
+      </body>
     </html>
   );
 }
